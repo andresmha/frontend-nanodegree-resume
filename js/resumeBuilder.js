1bda106587189c29d7293674d059933a574bc855
Andres Hernandez <andru.h@gmail.com>
2017-05-18 12:49:02 -0500

    Objects Created in resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,74 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//DATA Definition
+var work = {
+	jobs: [
+		{
+			employer: "Inalambrik",
+			title: "Project Manager",
+			location: "Guayaquil - Ecuador",
+			dates: "2016 - current",
+			description: ""
+		},
+		{
+			employer: "Batan",
+			title: "Software Development Manager",
+			location: "Guayaquil - Ecuador",
+			dates: "2010 - 2016",
+			description: ""
+		}
+	]
+};
+
+var projects = {
+	projects: [
+		{
+			title: "",
+			dates: "",
+			description: "",
+			images: []
+		}
+	]
+};
+
+var bio = {
+	name: "Andres Hernandez",
+	role: "Front End Developer",
+	welcomeMessage: "Hello World",
+	biopic: "",
+	contacts: {
+		mobile: "",
+		email: "",
+		github: "",
+		twitter: "",
+		location: ""
+	},
+	skills: ["HTML5", "CSS3", "JavaScript"]
+};
+
+var education = { 
+	schools: [
+		{
+			name: "",
+			location: "",
+			degree: "",
+			dates: "",
+			url: "",
+			majors: [],
+		}
+	],
+	onlineCourses: [
+		{
+			title: "",
+			school: "",
+			dates: "",
+			url: ""
+		}
+	]
+};
+
+
+//Interaction
+
+//Skills
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+}
